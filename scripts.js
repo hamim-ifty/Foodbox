599afc9e389d5cfab63ca8b6f83dfc1b3ff807be
hamim-ifty <hifty24@students.oamk.fi>
2024-10-10 16:06:54 +0300

    1.2

--- scripts.js
+++ scripts.js
@@ -3,5 +3,4 @@ function order(item) {
     alert(`You have ordered: ${item}`);
 }
 
-/* Add actual images to the images/ folder with the filenames specified in the code */ 
-hamim 
+/* Add actual images to the images/ folder with the filenames specified in the code */
